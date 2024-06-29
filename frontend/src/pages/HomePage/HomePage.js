26f0bac7ee04cf9372a139083f45552db682a60e
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-06-29 11:33:33 +0530

    backend done

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -4,24 +4,54 @@ import './HomePage.css';
 
 const HomePage = () => {
   const [fileUrl, setFile] = useState("");
+  const [filedata, setFileData] = useState("");
   function getURL(event) {
     var tmppath = event.target.files[0];
     setFile(tmppath);
   }
+
+  const arrayBufferToBase64 = (buffer) => {
+    let binary = '';
+
+    const bytes = new Uint8Array(buffer)
+
+    const len = bytes.length
+
+    for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode(bytes[i])
+    }
+
+    return window.btoa(binary) 
+  }
+
   async function fileSubmitted() {
     const blob = new Blob([fileUrl], { type: fileUrl.type });
     console.log(blob);
     console.log(fileUrl);
-    const file = await fetch('http://localhost:3000/upload-video', {
+    const arrayBuffer = await blob.arrayBuffer()
+    const base64 = arrayBufferToBase64(arrayBuffer)
+    
+    console.log("This is the base64 string")
+    console.log(base64)
+
+    const file = await fetch('http://localhost:3000/upload', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ blob: blob.arrayBuffer, name: fileUrl.name }),
+      body: JSON.stringify({ 
+        blob: base64, name: fileUrl.name, type: fileUrl.type
+      }),
     })
+
+    // setFileData(JSON.stringify(file));
+    console.log(file.text());
+    // const filedata = document.querySelector('.result-container').innerHTML = await file.text();
     
-    console.log(await file.json());
+    console.log(JSON.stringify(file));
   }
+
+
   return (
     <div className='homepage'>
       <div className='main-container'>
@@ -35,7 +65,7 @@ const HomePage = () => {
             <div id='text'></div>
           </div>
           <div className='result-container'>
-            respons
+            {filedata}
           </div>
         </div>
       </div>
