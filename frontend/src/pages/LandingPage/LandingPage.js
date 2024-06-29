7ee5f03733b85a5678cdd32c7119caea88a342c3
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-06-29 14:47:25 +0530

    landing

--- frontend/src/pages/LandingPage/LandingPage.js
+++ frontend/src/pages/LandingPage/LandingPage.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import './LandingPage.css'
+import { ArrowIcon, LogoIcon } from '../../icons/icons'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className='landing-page'>
+      <div className='landing-page-logo'>
+        <LogoIcon />
+        <div>Clarif.ai</div>
+      </div>
+      <div className='content'>
+        <div>Revolutionising Content Moderation with AI
+        </div>
+        <button>Explore more<ArrowIcon /></button>
+      </div>
+    </div>
   )
 }
 
